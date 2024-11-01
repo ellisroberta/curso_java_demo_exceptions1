aae6916f5f23ec619efbb98415d1848d30e6ce04
ellisroberta <ellisroberta@hotmail.com>
2024-11-01 09:57:15 -0300

    organizacao das pastas, edicao do .gitignore e do README, realizacao dos exercicios demo

--- application/src/application/Program1.java
+++ application/src/application/Program1.java
@@ -3,19 +3,9 @@ package application;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
-// Pilha de chamadas de métodos
-public class Program2 {
+// Estrutura try-catch
+public class Program1 {
     public static void main(String[] args) {
-        method1();
-        System.out.println("End of program");
-    }
-    public static void method1() {
-        System.out.println("***METHOD1 START***");
-        method2();
-        System.out.println("***METHOD1 END***");
-    }
-    public static void method2() {
-        System.out.println("***METHOD2 START***");
         Scanner sc = new Scanner(System.in);
         try {
             String[] vect = sc.nextLine().split(" ");
@@ -24,13 +14,11 @@ public class Program2 {
         }
         catch (ArrayIndexOutOfBoundsException e) {
             System.out.println("Invalid position!");
-            e.printStackTrace();
-            sc.next();
         }
         catch (InputMismatchException e) {
             System.out.println("Input error");
         }
+        System.out.println("End of program");
         sc.close();
-        System.out.println("***METHOD2 END***");
     }
 }
--- application/src/application/Program3.java
+++ application/src/application/Program3.java
@@ -1,4 +1,27 @@
 package application;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+// Bloco finally
 public class Program3 {
+    public static void main(String[] args) {
+        File file = new File("C:\\temp\\in.txt");
+        Scanner sc = null;
+        try {
+            sc = new Scanner(file);
+            while (sc.hasNextLine()) {
+                System.out.println(sc.nextLine());
+            }
+        }
+        catch (IOException e) {
+            System.out.println("Error opening file: " + e.getMessage());
+        }
+        finally {
+            if (sc != null) {
+                sc.close();
+            }
+        }
+    }
 }
